ed36ca4a29c51d43383d9f3644ed13d08c2b4c91
sota <11698569-jsulopzs@users.noreply.gitlab.com>
2025-02-17 18:31:43 +0100

    Achordions added to keyboard layout.

--- EaRm6/rules.mk
+++ EaRm6/rules.mk
@@ -1,3 +1,4 @@
+
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
@@ -7,3 +8,7 @@ ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
+
+
+# Custom codes
+SRC += features/achordion.c
