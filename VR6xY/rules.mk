053a7a5eda346820ba7022e58d511fe2a326e33e
jsulopzs <jesus.lopez@datons.ai>
2025-06-01 20:29:06 +0200

    🔧 Enable TAP_DANCE and adjust formatting in rules.mk

--- VR6xY/rules.mk
+++ VR6xY/rules.mk
@@ -2,7 +2,6 @@ CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
-TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
 
 # Custom codes
@@ -14,7 +13,8 @@ SRC += features/select_word.c
 SRC += features/sentence_case.c
 SRC += features/custom_shift_keys.c
 
-REPEAT_KEY_ENABLE = yes	
+REPEAT_KEY_ENABLE = yes
 LAYER_LOCK_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
